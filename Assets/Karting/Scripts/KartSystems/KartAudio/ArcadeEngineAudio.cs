b90d612b4634265cc7e7e4858a11f7014696485c
SSB4455 <741086@gmail.com>
2020-07-06 17:53:42 +0800

    2.0.2 2020-06-09 Use Unity 2019.4.0f1.

--- Assets/Karting/Scripts/KartSystems/KartAudio/ArcadeEngineAudio.cs
+++ Assets/Karting/Scripts/KartSystems/KartAudio/ArcadeEngineAudio.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
 
 namespace KartGame.KartSystems
 {
@@ -13,6 +14,16 @@ namespace KartGame.KartSystems
         public AudioSource IdleSound;
         [Tooltip("What audio clip should play when the kart moves around?")]
         public AudioSource RunningSound;
+        [Tooltip("Maximum Volume the running sound will be at full speed")]
+        [Range(0.1f, 1.0f)]public float RunningSoundMaxVolume = 1.0f;
+        [Tooltip("Maximum Pitch the running sound will be at full speed")]
+        [Range(0.1f, 2.0f)] public float RunningSoundMaxPitch = 1.0f;
+        [Tooltip("What audio clip should play when the kart moves in Reverse?")]
+        public AudioSource ReverseSound;
+        [Tooltip("Maximum Volume the Reverse sound will be at full Reverse speed")]
+        [Range(0.1f, 1.0f)] public float ReverseSoundMaxVolume = 0.5f;
+        [Tooltip("Maximum Pitch the Reverse sound will be at full Reverse speed")]
+        [Range(0.1f, 2.0f)] public float ReverseSoundMaxPitch = 0.6f;
 
         ArcadeKart arcadeKart;
 
@@ -23,10 +34,23 @@ namespace KartGame.KartSystems
 
         void Update()
         {
-            float kartSpeed     = arcadeKart != null ? arcadeKart.LocalSpeed() : 0;
-            RunningSound.volume = Mathf.Lerp(.1f, 1, kartSpeed / 2);
-            RunningSound.pitch  = Mathf.Lerp(.8f, 1.4f, kartSpeed / 4 + Mathf.Sin(Time.time) * .1f);
-            IdleSound.volume    = Mathf.Lerp(.5f, 0, kartSpeed);
+            float kartSpeed     = arcadeKart != null ? arcadeKart.LocalSpeed() : 0.0f;
+            IdleSound.volume    = Mathf.Lerp(0.6f, 0.0f, kartSpeed * 4);
+
+            if (kartSpeed < 0.0f)
+            {
+                // In reverse
+                RunningSound.volume = 0.0f;
+                ReverseSound.volume = Mathf.Lerp(0.1f, ReverseSoundMaxVolume, -kartSpeed * 1.2f);
+                ReverseSound.pitch = Mathf.Lerp(0.1f, ReverseSoundMaxPitch, -kartSpeed + (Mathf.Sin(Time.time) * .1f));
+            }
+            else
+            {
+                // Moving forward
+                ReverseSound.volume = 0.0f;
+                RunningSound.volume = Mathf.Lerp(0.1f, RunningSoundMaxVolume, kartSpeed * 1.2f);
+                RunningSound.pitch = Mathf.Lerp(0.3f, RunningSoundMaxPitch, kartSpeed + (Mathf.Sin(Time.time) * .1f));
+            }
         }
     }
 }
